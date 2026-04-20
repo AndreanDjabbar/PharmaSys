a8e7e4953cab973286197de7e6e7c64e377c6ab3
Andrean Gusman Djabbar <Andrean.djabbar@kpsg.com>
2026-04-20 11:43:17 +0700

    Add get user by email or username repo func

--- server/src/repository/user.repository.js
+++ server/src/repository/user.repository.js
@@ -1,18 +1,45 @@
 import postgreSQL from "../config/postgres.config.js";
+import { PrismaClient } from "../../prisma/generated/prisma/index.js";
+
+const prisma = new PrismaClient();
 
 class UserRepository {
   static async getByEmail(email) {
-    const [user] = await postgreSQL`
-            SELECT * FROM public."User" 
-            WHERE LOWER(email) = LOWER(${email})
-        `;
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    return user;
+  }
+
+  static async getUserByEmailOrUsername(emailOrUsername) {
+    const user = await prisma.user.findFirst({
+      where: {
+        OR: [
+          { email: emailOrUsername },
+          { username: emailOrUsername },
+        ],
+      },
+    });
+    return user;
+  }
+
+  static async getByUsername(username) {
+    const user = await prisma.user.findUnique({
+      where: {
+        username: username,
+      },
+    });
     return user;
   }
 
   static async getById(id) {
-    const [user] = await postgreSQL`
-            SELECT * FROM public."User" WHERE id = ${id}
-        `;
+    const user = await prisma.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
     return user;
   }
 }
