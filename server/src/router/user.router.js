05537ceacd7eac215c395027e70d059498a30cc3
Andrean Gusman Djabbar <Andrean.djabbar@kpsg.com>
2026-04-20 13:40:36 +0700

    Clean unnecessary code

--- server/src/router/user.router.js
+++ server/src/router/user.router.js
@@ -8,50 +8,17 @@ import timeout from "connect-timeout";
 
 import { 
   createStaffSchema ,
-  createTenantAdminSchema,
   loginSchema,
-  registerSchema,
-  registerOTPCodeSchema,
-  forgotPasswordEmailSchema,
-  forgotPasswordResetSchema,
   updateStaffSchema
 } from "../validation/user.validation.js";
 
 const router = express.Router();
 
 router.post(
-    "/register", 
-    timeout('10s'),
-    validateSchema(registerSchema),
-    catchAsync(UserController.registerController)
-);
-router.post(
-    "/verify/register-token", 
-    timeout('5s'),
-    catchAsync(UserController.verifyRegisterTokenController)
-);
-router.post(
-    "/verify/register-otp", 
-    timeout('5s'),
-    validateSchema(registerOTPCodeSchema), 
-    catchAsync(UserController.verifyRegisterOtpController)
-);
-router.post(
-    "/forgot-password/email-verification", 
-    timeout('8s'),
-    validateSchema(forgotPasswordEmailSchema), 
-    catchAsync(UserController.forgotPasswordEmailVerificationController)
-);
-router.post(
-    "/forgot-password/link-verification", 
-    timeout('5s'),
-    catchAsync(UserController.forgotPasswordLinkVerificationController)
-);
-router.post(
-    "/forgot-password/reset-password",
-    timeout('5s'),
-    validateSchema(forgotPasswordResetSchema), 
-    catchAsync(UserController.forgotPasswordResetController)
+    "/login", 
+    timeout('2s'),
+    validateSchema(loginSchema),
+    catchAsync(UserController.loginController)
 );
 router.post(
     "/logout", 
@@ -66,32 +33,18 @@ router.get(
   catchAsync(UserController.getMyUserDataController)
 );
 router.post(
-    "/login", 
-    timeout('2s'),
-    validateSchema(loginSchema),
-    catchAsync(UserController.loginController)
-);
-router.post(
-  "/tenant",
-  timeout('10s'),
-  validateToken,
-  validateSchema(createTenantAdminSchema),
-  authorizedRoles("Developer"),
-  catchAsync(UserController.createTenantController)
-);
-router.post(
   "/staff", 
   validateToken,
   timeout('8s'),
   validateSchema(createStaffSchema),
-  authorizedRoles("ADMIN", "Developer"), 
+  authorizedRoles("ADMIN", "DEVELOPER"), 
   catchAsync(UserController.createStaffController)
 );
 router.delete(
   "/staff/:id",
   timeout('5s'),
   validateToken,
-  authorizedRoles("ADMIN", "Developer"), 
+  authorizedRoles("ADMIN", "DEVELOPER"), 
   catchAsync(UserController.deleteStaffController)
 );
 router.put(
@@ -99,14 +52,8 @@ router.put(
   timeout('5s'),
   validateToken, 
   validateSchema(updateStaffSchema),
-  authorizedRoles("ADMIN", "Developer"), 
+  authorizedRoles("ADMIN", "DEVELOPER"), 
   catchAsync(UserController.updateStaffController)
 );
-router.get(
-  "/restaurant/:restaurantId/staff", 
-  timeout('3s'),
-  validateToken, 
-  catchAsync(UserController.getStaffByRestaurantIdController)
-)
 
 export default router;
